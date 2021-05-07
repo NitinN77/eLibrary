bfa5a30b5e8bf081169bfef6ad426c002e2ac3db
Nitin N <nitinnarayanan77@gmail.com>
2021-05-07 10:29:15 +0530

    security fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD0_ODsO8mGpUBWwulVYeAG5QBWdkbw5cg",
-    authDomain: "e-library-af799.firebaseapp.com",
-    projectId: "e-library-af799",
-    storageBucket: "e-library-af799.appspot.com",
-    messagingSenderId: "155626648685",
-    appId: "1:155626648685:web:b1235f7212068704edfb64",
-    measurementId: "G-WX4WM85VFV"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
