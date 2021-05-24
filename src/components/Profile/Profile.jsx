41846ed178a0e932d65c7c9b009487330b13adc3
Nitin N <nitinnarayanan77@gmail.com>
2021-05-24 12:03:06 +0530

    profile update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,43 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import useStyles from "./styles";
 import readsvg from "../../assets/undraw_Reading.svg";
-import {Tabs, Tab, AppBar} from '@material-ui/core'
+import {
+  Tabs,
+  Tab,
+  AppBar,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Grid,
+  Button,
+} from "@material-ui/core";
 import { useStateValue } from "../../StateProvider";
-import {Link} from 'react-router-dom'
-import { db } from '../../firebase'
+import { Link } from "react-router-dom";
+import { db } from "../../firebase";
 
 const Profile = () => {
   const classes = useStyles();
-  const [{ user }, ] = useStateValue();
+  const [{ user }] = useStateValue();
   const [value, setValue] = useState(0);
   const [bhistory, setBhistory] = useState([]);
-  const handleTabs = (e,val) => {
-      setValue(val)
-  }
+  const handleTabs = (e, val) => {
+    setValue(val);
+  };
 
   const fetchbhistory = async () => {
     let rdata = await db.collection("bhistory").doc(user.email).get();
-    rdata = rdata.data().history
-    let a = rdata.map(b => b.name)
+    rdata = rdata.data().history;
+    let a = rdata
     a = a.filter((item, i, ar) => ar.indexOf(item) === i);
-    setBhistory(a)
-  }
+    setBhistory(a);
+  };
 
   useEffect(() => {
     if (user) {
       fetchbhistory();
     }
-  }, [user])
-
+  }, [user]);
 
   return (
     <>
@@ -49,42 +58,60 @@ const Profile = () => {
           />
         </div>
       </div>
-      {user? 
+      {user ? (
         <div>
-            <AppBar position="static">
-                <Tabs value={value} onChange={handleTabs}>
-                    <Tab label="BORROWING HISTORY" />
-                    <Tab label="item 2" />
-                    <Tab label="item 3" />
-                </Tabs>
-            </AppBar>
-            <TabPanel value={value} index={0}>
-              {bhistory && bhistory.map(book => (<p>{book}</p>))}
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-          
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              
-            </TabPanel>
-        </div>
-        : <div><h3>You must <Link to="/login">Log in</Link></h3></div> }
+          <AppBar position="static">
+            <Tabs value={value} onChange={handleTabs}>
+              <Tab label="BORROWING HISTORY" />
+              {/* <Tab label="item 2" />
+                    <Tab label="item 3" /> */}
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            <Grid item container justify="center" spacing={3} lg={12} style={{marginTop: '30px'}}> 
+              {bhistory && bhistory.map((book) => (
+                <Card className={classes.cardroot}>
+                  <CardMedia
+                    className={classes.media}
+                    image={book.media.source}
+                    title={book.name}
+                  />
 
+                  <div className={classes.cardContent1}>
+                    <CardContent>
+                      <div className={classes.cardContent}>
+                        <Typography variant="h5" gutterBottom>
+                          {book.name}
+                        </Typography>
+                      </div>
+                      <Typography
+                        dangerouslySetInnerHTML={{ __html: book.description }}
+                        variant="body2"
+                        color="textSecondary"
+                      />
+                    </CardContent>
+                  </div>
+                </Card>
+              ))}
+            </Grid>
+          </TabPanel>
+          <TabPanel value={value} index={1}></TabPanel>
+          <TabPanel value={value} index={2}></TabPanel>
+        </div>
+      ) : (
+        <div>
+          <h3>
+            You must <Link to="/login">Log in</Link>
+          </h3>
+        </div>
+      )}
     </>
   );
 };
 
 const TabPanel = (props) => {
-    const {children, value, index} = props
-    return(
-        <div>
-            {
-                value===index && (
-                    <h1>{children}</h1>
-                )
-            }
-        </div>
-    )
-}
+  const { children, value, index } = props;
+  return <div>{value === index && <h1>{children}</h1>}</div>;
+};
 
 export default Profile;
--- src/components/Profile/styles.js
+++ src/components/Profile/styles.js
@@ -12,4 +12,35 @@ export default makeStyles(() => ({
       flexDirection: 'row',
       justifyContent: 'space-between'
     },
+    cardroot: {
+      maxWidth: '100%',
+      height: '300px',
+      display: 'flex',
+      marginBottom: '30px'
+    },
+    media: {
+      display: 'flex',
+      width: '300px',
+      justifyContent: 'flex-start'
+    },
+    cardActions: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    cardContent: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      flexDirection: 'column',
+  
+    },
+    cardContent1: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      width: '1200px',
+    },
+    topsection: {
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
 }));
+
